92b5777a9677590803dc8f6c42bc58a3613ca832
wangliarchi <wangliarchi@gmail.com>
2013-10-11 09:52:42 -0700

    Update FindRange.java

--- Assignment2/FindRange.java
+++ Assignment2/FindRange.java
@@ -1,26 +1,53 @@
 /*
  * File: FindRange.java
- * Name:  
- * Section Leader: 
+ * Name: wangli 
+ * Section Leader: jianbin ray
+ * helper: jianbin, http://cs106a.tumblr.com
  * --------------------
  * This file is the starter file for the FindRange problem.
+ * input some numbers until the sentinel, the program will display the smallest and largest.
+ * while when the user put an sentinel number at first, the program will send a alert message.
  */
 
-import acm.program.*;
 
+import acm.program.*;
 public class FindRange extends ConsoleProgram {
+/* Define a SENTINEL number for two use,
+ * 1.if the user input sentinel number at first, the program will send an alert message
+ * 2.the input number could be stopped when the user put in sentinel number, so the screen will
+ * display the smallest and largest number.
+ */
+	private static final int SENTINEL = 0;
+
 	public void run() {
-		/* You fill this in */
 		println("This program finds the largest and smallest numbers");
-		int num = smallest;
-		int 
-		while(true){
-			int a = readInt("?");
-			if(a = SENTINEL) break;
-			i
+/* send the alert message brick */
+		int num1 = readInt("?");
+		if( num1 == SENTINEL) {
+		println("you have put in an end number");
 		}
-		
-		
+/*   */
+		else{
+		int smallest = num1;
+		int largest = num1;
+		while(true){
+			int number = readInt("?");
+			if(number == SENTINEL) break;
+			if(number !=SENTINEL&&number<=smallest){
+				smallest = number;
+		}
+			if(number>=largest){
+				largest = number;
+				}
+			}
+		println("the smallest number is"+":" + smallest);
+		println("the largest numer is"+":" + largest );
+			
+		}
 	}
 }
+
+
+
+
 
