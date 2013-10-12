966e8d0d22616550386e088297c1f85a94a2a2be
wangliarchi <wangliarchi@gmail.com>
2013-10-11 17:28:46 -0700

    Update Pyramid.java

--- Assignment2/Pyramid.java
+++ Assignment2/Pyramid.java
@@ -1,7 +1,7 @@
-/*
- * File: Pyramid.java
- * Name: 
- * Section Leader: 
+* File: Pyramid.java
+ * Name: wang li
+ * helpes by: Jianbin Ray. refer to Dianna.
+ * Section Leader: Jianbin Ray
  * ------------------
  * This file is the starter file for the Pyramid problem.
  * It includes definitions of the constants that match the
@@ -24,27 +24,36 @@ public class Pyramid extends GraphicsProgram {
 
 /** Number of bricks in the base of the pyramid */
 	private static final int BRICKS_IN_BASE = 14;
-	
-	public void run() {
-		putBricksInRow();
-		int x = (getWidth() - BRICK_WIDTH * 14) / 2;
-		int y = getHeight() - BRICK_HEIGHT;
-	}  
-
- 
-	private void putBricksInRow(){
-		int row = 14;
-		
-		for(int j=0; j<row; j++){
-			for(int i = 0; i<BRICKS_IN_BASE; i++){ 
-			   int x = x+BRICK_WIDTH * i / 2;
-			   int y = y-BRICK_HEIGHT * j;
-	           GRect rec= new GRect(x, y, BRIC
-			K_WIDTH, BRICK_HEIGHT ) ;
-		       add(rec);
-		
+/* draw bricks row by row, in each row, the bricks of one row 
+ * reduced by one per row, until there is only one brick in the prominent point 
+ * of the pyramid.  
+ */
+	public void run(){
+/* calculate the original coordinate of the first bricks in the base row.  */
+	double coorx = (getWidth() - BRICK_WIDTH * BRICKS_IN_BASE) / 2;
+	double coory = getHeight() - BRICK_HEIGHT;
+	int bricksInRow =14;
+/* define a loop, include inner for loop and a outer while loop.
+ * the inner loop aims for draw bricks one by one in a row.
+ * the outer loop aims for reduce the numbers of bricks by one per row
+ * until there is only one brick left in a row , the loop ends.
+ */
+	while (bricksInRow > 0){
+		for (int i=0; i<bricksInRow;i++){
+			double x = coorx + BRICK_WIDTH * i;
+			double y = coory;
+			GRect rec = new GRect(x, y, BRICK_WIDTH, BRICK_HEIGHT);
+			add(rec);	
+		}
+		bricksInRow--;
+/* the coordinate of the first bricks of each row varies, so we should change it after 
+ * complete every inner loop.
+ */
+		coorx += BRICK_WIDTH/2;
+		coory -=BRICK_HEIGHT;	
 	}
-	
+	 }
+	  }
 	
 
 	
